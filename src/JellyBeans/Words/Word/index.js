fabfd5ea4a68f273f7b94f8e1738ae916b6c8f00
Nate Clark <n8@n8io.com>
2020-10-27 17:37:44 -0400

    🤜 Bump to v0.2.0

--- src/JellyBeans/Words/Word/index.js
+++ src/JellyBeans/Words/Word/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { number, string } from 'prop-types';
 import React, { useEffect, useState } from 'react';
 import { niceWordLengths } from '../../../constants/niceWordLengths';
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,2 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { cleanup } from '@testing-library/react';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import 'jest-styled-components';
-
-beforeEach(cleanup);
+import '@testing-library/react';
